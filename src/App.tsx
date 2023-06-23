4fc2acf239ec67f65fefbaaeee7c5c4727c62360
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-23 15:42:01 -0500

    Basic scoreboard functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import './App.css';
+import './styles/App.css';
+import Scoreboard from './components/Scoreboard';
+import { useState } from 'react';
+interface State {
+	currentScore: number;
+	highScore: number;
+}
 
 function App() {
+	const initialState: State = {
+		currentScore: 0,
+		highScore: 0,
+	};
+	let [state, setState] = useState(initialState);
+	const correctChoice = () => {
+		const newHS =
+			state.currentScore + 1 > state.highScore
+				? state.highScore + 1
+				: state.highScore;
+		setState({
+			currentScore: state.currentScore + 1,
+			highScore: newHS,
+		});
+	};
+	const incorrectChoice = () => {
+		setState({
+			currentScore: 0,
+			highScore: state.highScore,
+		});
+	};
 	return (
-		<>
-			<div>App</div>
-		</>
+		<div className="game">
+			<Scoreboard
+				currentScore={state.currentScore}
+				highScore={state.highScore}
+			/>
+			<button onClick={correctChoice}>Correct</button>
+			<button onClick={incorrectChoice}>inCorrect</button>
+		</div>
 	);
 }
 
