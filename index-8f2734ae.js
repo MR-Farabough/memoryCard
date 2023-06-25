16fd77deef9cec0fd66437d65c08352be73e07a8
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-24 19:58:52 -0500

    Image error fix

--- index-8f2734ae.js
+++ index-8f2734ae.js
@@ -8995,7 +8995,6 @@ nc(), (bi.exports = ge);
 var Td = bi.exports,
 	Bi = Td;
 (Ql.createRoot = Bi.createRoot), (Ql.hydrateRoot = Bi.hydrateRoot);
-import blueButterfly from './assets/blue-butterfly-b464468e.jpg';
 const Rd = ({ currentScore: e, highScore: n }) =>
 		me.jsxs('div', {
 			className: 'scoreBoard',
@@ -9004,7 +9003,7 @@ const Rd = ({ currentScore: e, highScore: n }) =>
 				me.jsxs('p', { className: 'text', children: ['High Score: ', n] }),
 			],
 		}),
-	Od = blueButterfly,
+	Od = './assets/blue-butterfly-b464468e.jpg',
 	Md = './assets/blue-green-e1c7f743.jpg',
 	jd = './assets/blue-jay.jpg-eebf1315.webp',
 	Dd = './assets/blue-moon-6db95b4f.jpg',
